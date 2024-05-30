21567ae2fb5d1d088a155618b1ade032dcd56d5f
Juan Ignacio Espinoza Retamal <jiespinozar@uc.cl>
2024-05-30 18:09:13 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='ironman',
+    version='0.0.1',
+    author='Juan Ignacio Espinoza-Retamal',
+    author_email='jiespinozar@uc.cl',
+    description='Joint Fit Rossiter McLaughlin Data with photometry and out-of-transit radial velocities',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jiespinozar/ironman',
+    packages=find_packages(),
+    install_requires=['numpy','scipy','pandas','batman-package','rmfit','dynesty','astropy'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Astronomy',
+    ],
+    keywords='Astronomy, Obliquities, Radial Velocities',
+    license='MIT',
+    include_package_data=True,
+    python_requires='>=3.6',
+)
