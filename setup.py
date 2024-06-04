bcba0809f012103eb4145a90836adbb146494e04
Juan Ignacio Espinoza Retamal <jiespinozar@uc.cl>
2024-06-05 00:28:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='ironman-package',
-    version='0.0.3',
+    version='0.0.4',
     author='Juan Ignacio Espinoza-Retamal',
     author_email='jiespinozar@uc.cl',
     description='Joint Fit Rossiter McLaughlin Data with photometry and out-of-transit radial velocities',
